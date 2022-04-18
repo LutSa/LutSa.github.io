5539ae8b5589bdb2a6a72ae1b17708c3c790c626
Wu Jinyi <wujinyi@umich.edu>
2022-04-18 17:16:10 -0400

    roll bak
    roll bak

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './my-blog-starter/public',
-      url: ['http://localhost:9000']
+      staticDistDir: './public',
+      url: ['https://lutsa.github.io/']
     },
     upload: {
       target: 'temporary-public-storage',
